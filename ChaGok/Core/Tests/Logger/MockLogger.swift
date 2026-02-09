14c57e485da8d6d6183a3384bd53338244d5322f
tomchoi <qjatn0545@naver.com>
2026-02-09 14:25:56 +0900

    feat: 로깅 시스템 구현

---
+++ ChaGok/Core/Tests/Logger/MockLogger.swift
@@ -0,0 +1,14 @@
+import Foundation
+@testable import Core
+
+enum MockLogger: AppLoggerProtocol {
+    nonisolated(unsafe) static var recordedLogs: [(level: LogLevel, message: String)] = []
+
+    static func log(_ level: LogLevel, message: String, file: String, function: String, line: Int) {
+        recordedLogs.append((level, message))
+    }
+
+    static func reset() {
+        recordedLogs = []
+    }
+}
