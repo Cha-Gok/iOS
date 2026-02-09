883147d9cfaa872be76faf36fe9695cf3c4c0d09
tomchoi <qjatn0545@naver.com>
2026-02-09 16:51:57 +0900

    docs: StorageInfo 구조체에 대한 주석 추가

--- ChaGok/Domain/Sources/Entity/StorageInfo.swift
+++ ChaGok/Domain/Sources/Entity/StorageInfo.swift
@@ -7,11 +7,16 @@
 
 import Foundation
 
+/// StorageInfo는 기기의 저장소 정보를 담고 있는 구조체입니다.
+/// - totalBytes: 저장소의 전체 바이트 수.
+/// - freeBytes: 저장소의 남은(여유) 바이트 수.
+/// - appUsedBytes: 앱이 사용한 바이트 수.
+/// - usageRatio: 앱 사용량 비율.
 public struct StorageInfo {
     public let totalBytes: Int64
     public let freeBytes: Int64
     public let appUsedBytes: Int64
-    
+
     public var usageRatio: Double {
         Double(appUsedBytes) / Double(totalBytes)
     }
