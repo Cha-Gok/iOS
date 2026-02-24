a47a3879583b967cb3444f1aba5684b8a6f9093c
김용해 <yoho1019@gmail.com>
2026-02-24 22:12:12 +0900

    refactor: folder  수정

--- ChaGok/Domain/Sources/Entities/Folder.swift
+++ ChaGok/Domain/Sources/Entities/Folder.swift
@@ -5,16 +5,19 @@ public struct Folder {
     public let path: URL
     public let name: String
     public let createdAt: Date
-
+    public let content: [VoiceNote]
+    
     public init(
         id: String,
         path: URL,
         name: String,
-        createdAt: Date
+        createdAt: Date,
+        content: [VoiceNote]
     ) {
         self.id = id
         self.path = path
         self.name = name
         self.createdAt = createdAt
+        self.content = content
     }
 }
