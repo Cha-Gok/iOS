2041441f82d52bac04310d0567c3636796411ed1
김용해 <yoho1019@gmail.com>
2026-02-25 19:11:03 +0900

    refactor: async/await 비동기 처리 과정을 위한 Sendable 처리

--- ChaGok/Domain/Sources/UseCases/DeleteFolderUseCase.swift
+++ ChaGok/Domain/Sources/UseCases/DeleteFolderUseCase.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// 폴더 삭제 유스케이스 프로토콜.
 /// 지정한 폴더의 실제 디렉토리와 CoreData 모델을 삭제합니다.
-public protocol DeleteFolderUseCase {
+public protocol DeleteFolderUseCase: Sendable {
     /// ID로 특정 폴더를 삭제합니다.
     /// - Parameter id: 삭제할 폴더의 ID
     /// - Throws: 폴더 삭제 실패 시
--- ChaGok/Domain/Sources/UseCases/UpdateFolderUseCase.swift
+++ ChaGok/Domain/Sources/UseCases/UpdateFolderUseCase.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// 폴더 정보 업데이트 유스케이스 프로토콜.
 /// 폴더 이름 변경 등 기존 폴더의 정보를 수정합니다.
-public protocol UpdateFolderUseCase {
+public protocol UpdateFolderUseCase: Sendable {
     /// 폴더 정보를 업데이트합니다.
     /// - Parameter folder: 업데이트할 `Folder` 엔티티
     /// - Returns: 업데이트된 `Folder` 엔티티
@@ -13,11 +13,11 @@ public protocol UpdateFolderUseCase {
 public struct DefaultUpdateFolderUseCase: UpdateFolderUseCase {
 
     private let repository: FolderRepository
-    
+
     public init(repository: FolderRepository) {
         self.repository = repository
     }
-    
+
     public func execute(_ folder: Folder) async throws -> Folder {
         try await repository.update(folder)
     }
