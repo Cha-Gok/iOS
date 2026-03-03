af1e0606e546a70c8b03ce8f3aef1b5193341835
김용해 <yoho1019@gmail.com>
2026-03-04 03:57:08 +0900

    feat: 마이크 권한 요청 UseCase - VoiceNoteRepository를 사용

---
+++ ChaGok/Domain/Sources/UseCases/Authority/CheckMicrophonePermissionUseCase.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+/// 온보딩 과정에서 마이크 권한 확인을 위한 유즈케이스
+public protocol CheckMicrophonePermissionUseCase: Sendable {
+    func execute() async throws
+}
+
+/// 온보딩에서 마이크 권한을 요청 또는 확인 합니다.
+public struct DefaultCheckMicrophonePermissionUseCase: CheckMicrophonePermissionUseCase {
+    private let repository: VoiceRecordRepository
+
+    init(repository: VoiceRecordRepository) {
+        self.repository = repository
+    }
+
+    public func execute() async throws {
+        try await repository.checkRecordingPermission()
+    }
+}
