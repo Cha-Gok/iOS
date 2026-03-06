5c015789e1808d943fae76e4368925d4795111b3
tomchoi <qjatn0545@naver.com>
2026-03-06 09:43:18 +0900

    feat: STTRepositoryError 열거형 추가
    - 음성 인식(STT) 리포지토리에서 발생할 수 있는 에러를 정의하는 STTRepositoryError 열거형 추가
    - 각 에러에 대한 설명을 제공하는 errorDescription 프로퍼티 구현

---
+++ ChaGok/Domain/Sources/Errors/STTRepositoryError.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+/// 음성 인식(STT) 리포지토리에서 발생할 수 있는 에러.
+public enum STTRepositoryError: Error, LocalizedError, Sendable {
+
+    /// 오디오 전사(Transcription) 실패.
+    case transcribeFailed
+
+    /// 알 수 없는 에러.
+    case unknown(Error)
+
+    public var errorDescription: String? {
+        switch self {
+        case .transcribeFailed:
+            return "음성 인식에 실패했습니다."
+        case .unknown:
+            return "알 수 없는 에러가 발생했습니다."
+        }
+    }
+}
