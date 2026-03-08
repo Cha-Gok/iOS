c27a8beba1907434df2a48fc01bb9bf49821abed
김용해 <yoho1019@gmail.com>
2026-03-09 02:12:32 +0900

    refactor(Domain): WasteBasketItem 엔티티 수정 1. UseCase, Repository Error Description을 적용하기 위함.

--- ChaGok/Domain/Sources/Entities/WasteBasketItem.swift
+++ ChaGok/Domain/Sources/Entities/WasteBasketItem.swift
@@ -14,12 +14,32 @@ public enum DeleteWasteBasketMethod: Sendable {
     case multiple(items: [WasteBasketItem])
     /// 개별 삭제
     case single(item: WasteBasketItem)
+
+    public var errorDescription: String {
+        switch self {
+        case .all:
+            "휴지통 전체 삭제를 실패하였습니다"
+        case .multiple:
+            "휴지통 다수 선택 삭제를 실패하였습니다"
+        case .single:
+            "휴지통 개별 삭제를 실패하였습니다"
+        }
+    }
 }
 
-/// 휴지통으로 이동 방식을 정의하는 열거 형
+/// 휴지통으로 이동 방식을 정의하는 열거형
 public enum MoveWasteBasketMethod: Sendable {
     /// 개별 이동
     case single(item: WasteBasketItem)
     /// 다수 선택 이동
     case multiple(items: [WasteBasketItem])
+
+    public var errorDescription: String {
+        switch self {
+        case .single:
+            "휴지통 개별 이동을 실패하였습니다"
+        case .multiple:
+            "휴지통 다수 선택 이동을 실패하였습니다"
+        }
+    }
 }
