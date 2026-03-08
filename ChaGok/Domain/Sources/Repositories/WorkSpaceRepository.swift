79ecdc5c55f6097fe3119c30b7fe72a6970fe694
김용해 <yoho1019@gmail.com>
2026-03-09 02:31:45 +0900

    refactor(Domain): WorkSpace 구현제 인터페이스 로직 수정 1. fetchURL을 통해 루트 URL 반환 (검증 로직은 제거합니다) 2. 기본 폴더 조회 및 Folder ENTITY 생성 (기본 폴더가 없을 경우 생성 합니다.)

--- ChaGok/Domain/Sources/Repositories/WorkSpaceRepository.swift
+++ ChaGok/Domain/Sources/Repositories/WorkSpaceRepository.swift
@@ -4,20 +4,15 @@ import Foundation
 /// 기본 폴더에 한해서만 Fetch 기능이 있습니다.
 public protocol WorkSpaceRepository: Sendable {
 
-    /// 루트 디렉토리 URL을 반환하거나, 존재하지 않으면 생성하여 반환합니다.
+    /// 루트 디렉토리 URL을 반환합니다
     /// - Returns: 루트 폴더 URL
-    /// - Throws: 루트 폴더 생성 실패 시
-    func fetchOrCreateRootDirectory() async throws -> URL
+    /// - Throws: 루트 URL 반환 실패 시
+    func fetchRootURL() async throws(WorkSpaceRootURLRepositoryError) -> URL
 
-    /// 휴지통 폴더 URL을 반환하거나, 존재하지 않으면 생성하여 반환합니다.
-    /// - Returns: 휴지통 폴더 URL
-    /// - Throws: 휴지통 폴더 생성 실패 시
-    func fetchOrCreateWasteBasketDirectory() async throws -> URL
-
-    /// 기본 폴더도를 반환 합니다.
+    /// 기본 폴더를 반환 합니다.
     /// fetchOrCreateRootDirectory 를 통해 반드시 루트 URL을 알아야 합니다.
     /// - Returns: Folder Entity 반환
     /// - Throws: 기본 폴더 생성 실패 시
-    @discardableResult func fetchOrCreateBasicFolder() async throws -> Folder
+    @discardableResult func fetchOrCreateBasicFolder() async throws(WorkSpaceBasicFolderRepositoryError) -> Folder
 
 }
