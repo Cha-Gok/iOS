8a1c889464ad655d51e9bfc3e6e1f915d8c98ffb
김용해 <yoho1019@gmail.com>
2026-03-11 20:26:19 +0900

    refactor(domain): 내부 UseCaseError 변환 코드 캡슐화 - switch 문법을 내부 UpdateFolderUseCaseError 캡슐화 했습니다. - 에러 케이스( invaildName ) 추가

--- ChaGok/Domain/Sources/Errors/Folders/UpdateFolderUseCaseError.swift
+++ ChaGok/Domain/Sources/Errors/Folders/UpdateFolderUseCaseError.swift
@@ -3,6 +3,8 @@ import Foundation
 public enum UpdateFolderUseCaseError: LocalizedError, Sendable {
     /// 작업 취소의 경우
     case cancelled
+    /// 유효하지 않음 이름의 경우
+    case invalidName
     /// 폴더를 찾을 수 없는 경우 (조회, 수정 시 발생)
     case notFound
     /// 동일한 이름의 폴더가 이미 존재하는 경우 (생성, 수정 시 발생)
@@ -16,6 +18,8 @@ public enum UpdateFolderUseCaseError: LocalizedError, Sendable {
         switch self {
             case .cancelled:
                 nil
+            case .invalidName:
+                "폴더 이름을 한 글자 이상 입력해 주세요."
             case .notFound:
                 "해당 폴더를 찾을 수 없습니다."
             case .duplicateName:
@@ -26,4 +30,20 @@ public enum UpdateFolderUseCaseError: LocalizedError, Sendable {
                 error.localizedDescription
         }
     }
+
+    public init(_ error: FolderRepositoryError) {
+        switch error {
+            case .cancelled:
+                self = .cancelled
+            case .duplicateName:
+                self = .duplicateName
+            case .notFound:
+                self = .notFound
+            case .updateFailed:
+                self = .updateFailed
+            default:
+                self = .unknown(error)
+        }
+    }
+
 }
