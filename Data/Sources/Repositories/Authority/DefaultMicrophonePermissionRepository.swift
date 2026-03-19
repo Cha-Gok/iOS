fad99c773e2a6f2d0b457e3eaf796c10da594752
tomchoi <qjatn0545@naver.com>
2026-03-19 18:15:04 +0900

    feat(data): DefaultMicrophonePermissionRepository, DefaultSTTPermissionRepository 구현체 추가

---
+++ Data/Sources/Repositories/Authority/DefaultMicrophonePermissionRepository.swift
@@ -0,0 +1,19 @@
+import Domain
+
+public struct DefaultMicrophonePermissionRepository: MicrophonePermissionRepository {
+    private let service: any MicrophonePermissionService
+
+    init(service: any MicrophonePermissionService) {
+        self.service = service
+    }
+
+    public func checkMicrophonePermission() async throws(MicrophonePermissionRepositoryError) -> PermissionStatus {
+        if Task.isCancelled { throw .cancelled }
+        return await service.checkPermission()
+    }
+
+    public func requestMicrophonePermission() async throws(MicrophonePermissionRepositoryError) -> PermissionStatus {
+        if Task.isCancelled { throw .cancelled }
+        return await service.requestPermission()
+    }
+}
