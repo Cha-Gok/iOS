9daeade333459325b3f241c99a831aea6b8191db
김용해 <yoho1019@gmail.com>
2026-03-23 06:18:13 +0900

    feat(data): Service 구현체 작성 - setUser는 기존 사용자로 만들기 위한 함수입니다. - getFirstUser는 신규 사용자인지 판단하는 함수입니다.

---
+++ Data/Sources/Infrastructure/OnBoarding/DefaultCheckFirstUserService.swift
@@ -0,0 +1,12 @@
+import Domain
+import Foundation
+
+public final class DefaultCheckFirstUserService: CheckFirstUserService {
+    public func setUser() {
+        UserDefaults.standard.set(true, forKey: Policy.isExistingUserKey)
+    }
+
+    public func getFirstUser() -> Bool {
+        UserDefaults.standard.bool(forKey: Policy.isExistingUserKey)
+    }
+}
