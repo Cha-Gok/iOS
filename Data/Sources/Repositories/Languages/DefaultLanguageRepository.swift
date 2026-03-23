86aa459158aa883fd40ed92bbd5b4c9fcf761576
김용해 <yoho1019@gmail.com>
2026-03-24 03:35:41 +0900

    refactor(data): swiftFormat 적용

--- Data/Sources/Repositories/Languages/DefaultLanguageRepository.swift
+++ Data/Sources/Repositories/Languages/DefaultLanguageRepository.swift
@@ -2,23 +2,23 @@ import Domain
 import Foundation
 
 public struct DefaultLanguageRepository: LanguageRepository {
-  private let service: any LanguageService
+    private let service: any LanguageService
 
-  public init(service: any LanguageService) {
-    self.service = service
-  }
-
-  public func fetchLanguage() async throws(FetchLanguagesRepositoryError) -> Language {
-    if Task.isCancelled { throw FetchLanguagesRepositoryError.cancelled }
-
-    guard let rawLanguage = try? service.fetchLanguage() else {
-      return .ko
+    public init(service: any LanguageService) {
+        self.service = service
     }
-    return Language(rawValue: rawLanguage) ?? .ko
-  }
 
-  public func saveLanguage(_ language: Language) async throws(SetLanguagesRepositoryError) {
-    if Task.isCancelled { throw .cancelled }
-    service.saveLanguage(language.rawValue)
-  }
+    public func fetchLanguage() async throws(FetchLanguagesRepositoryError) -> Language {
+        if Task.isCancelled { throw FetchLanguagesRepositoryError.cancelled }
+
+        guard let rawLanguage = try? service.fetchLanguage() else {
+            return .ko
+        }
+        return Language(rawValue: rawLanguage) ?? .ko
+    }
+
+    public func saveLanguage(_ language: Language) async throws(SetLanguagesRepositoryError) {
+        if Task.isCancelled { throw .cancelled }
+        service.saveLanguage(language.rawValue)
+    }
 }
