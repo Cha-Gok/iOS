ff99f2dd28b6c835b144a268481d3891fee6540e
김용해 <yoho1019@gmail.com>
2026-03-23 22:57:24 +0900

    refactor(data): FolderEntity 구현체 내부 리펙토링 - id를 통해 Entity 조회 함수 정의 (find, identityPredicate) - path가 제거되었습니다. - entityName 열거형 전환

--- Data/Sources/Infrastructure/CoreData/Entities/FolderEntity+CoreDataClass.swift
+++ Data/Sources/Infrastructure/CoreData/Entities/FolderEntity+CoreDataClass.swift
@@ -10,9 +10,6 @@ public final class FolderEntity: NSManagedObject {
     public var name: String
 
     @NSManaged
-    public var path: URL
-
-    @NSManaged
     public var createdAt: Date
 
     @NSManaged
@@ -45,16 +42,16 @@ public extension FolderEntity {
 
 extension FolderEntity: ManagedObjectMapping {
     public typealias DomainType = Folder
+    public typealias ChildType = VoiceNote
 
-    public convenience init(domain: Folder, context: NSManagedObjectContext) {
+    public convenience init(domain: DomainType, context: NSManagedObjectContext) {
         self.init(context: context)
         insert(from: domain)
     }
 
-    public func toDomain() -> Folder {
-        Folder(
+    public func toDomain() -> DomainType {
+        DomainType(
             id: id,
-            path: path,
             name: name,
             createdAt: createdAt,
             content: children,
@@ -71,27 +68,37 @@ extension FolderEntity: ManagedObjectMapping {
     public func insert(from domain: Folder) {
         id = domain.id
         name = domain.name
-        path = domain.path
         createdAt = domain.createdAt
         isDeletable = domain.isDeletable
         deletedAt = domain.deletedAt
     }
 
-    public static var entityName: String {
-        "FolderEntity"
+    public static var entityName: CoreDataEntityName {
+        .folder
     }
 
     public static var sortDescriptors: [NSSortDescriptor] {
         [NSSortDescriptor(keyPath: \FolderEntity.createdAt, ascending: true)]
     }
 
-    public static func identityPredicate(for domain: Folder) -> NSPredicate {
+    public static func identityPredicate(for domain: DomainType) -> NSPredicate {
         return NSPredicate(format: "id == %@", domain.id as CVarArg)
     }
 
+    public static func identityPredicate(byId id: DomainType.ID) -> NSPredicate {
+        return NSPredicate(format: "id == %@", id as CVarArg)
+    }
+
     public static func find(for domain: Folder, in context: NSManagedObjectContext) throws -> Self? {
-        let request = NSFetchRequest<Self>(entityName: entityName)
+        let request = NSFetchRequest<Self>(entityName: entityName.rawValue)
         request.predicate = identityPredicate(for: domain)
+        request.fetchLimit = 1
+        return try context.fetch(request).first
+    }
+
+    public static func find(byId id: DomainType.ID, in context: NSManagedObjectContext) throws -> Self? {
+        let request = NSFetchRequest<Self>(entityName: entityName.rawValue)
+        request.predicate = identityPredicate(byId: id)
         request.fetchLimit = 1
         return try context.fetch(request).first
     }
