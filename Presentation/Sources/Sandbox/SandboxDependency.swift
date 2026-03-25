b013f00338be5a5e0b471ed291a69e6474c99715
김용해 <yoho1019@gmail.com>
2026-03-26 06:13:37 +0900

    feat(presentation): 의존성 프로토콜 구현 - 테스트 할 UseCase를 정의 해주면 됩니다. - 아직 Infrastructure가 작성 되지 않은 UseCase는 넣지 않았습니다.

---
+++ Presentation/Sources/Sandbox/SandboxDependency.swift
@@ -0,0 +1,33 @@
+import Domain
+import Foundation
+
+/// Sandbox UI에서 테스트할 UseCase들을 주입받기 위한 프로토콜입니다.
+/// App 레이어의 Dependency Injection 단계에서 실제 구현체 또는 Mock을 주입합니다.
+public protocol SandboxDependency: Sendable {
+    // WorkSpace (구현체 미 구현)
+
+    // Authority (Check , Request)
+    var checkFirstLaunchUseCase: CheckFirstLaunchUseCase { get }
+    var checkMicrophonePermissionUseCase: CheckMicrophonePermissionUseCase { get }
+    var checkSTTPermissionUseCase: CheckSTTPermissionUseCase { get }
+
+    var requestMicrophonePermissionUseCase: RequestMicrophonePermissionUseCase { get }
+    var requestSTTPermissionUseCase: RequestSTTPermissionUseCase { get }
+
+    // WasteBasket (구현체 미 구현)
+
+    // VoiceNote (구현체 미 구현)
+
+    // Language
+    var fetchLanguageUseCase: FetchLanguageUseCase { get }
+    var selectLanguageUseCase: SelectLanguageUseCase { get }
+    // Folders UseCase
+    var createFolderUseCase: CreateFolderUseCase { get }
+    var readFolderUseCase: ReadFolderUseCase { get }
+    var updateFolderUseCase: UpdateFolderUseCase { get }
+
+    // Recoding UseCase
+    var startRecordingUseCase: StartRecordingUseCase { get }
+    var pauseRecordingUseCase: PauseRecordingUseCase { get }
+    var resumeRecordingUseCase: ResumeRecordingUseCase { get }
+}
