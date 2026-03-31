b7c48ea512ea97b81ee85439183860cf1c155b97
TomBumSuChoi <qjatn0545@naver.com>
2026-03-31 23:24:42 +0900

    refactor(data): CoreData 엔티티들에 새로운 매핑 요구사항 적용

--- Data/Sources/Infrastructure/CoreData/Entities/KeywordEntity+CoreDataClass.swift
+++ Data/Sources/Infrastructure/CoreData/Entities/KeywordEntity+CoreDataClass.swift
@@ -14,14 +14,14 @@ public final class KeywordEntity: NSManagedObject {
 }
 
 extension KeywordEntity: ManagedObjectMapping {
-    public typealias DomainType = Keyword
+    public typealias ModelType = Keyword
 
-    public convenience init(domain: DomainType, context: NSManagedObjectContext) {
+    public convenience init(model: ModelType, context: NSManagedObjectContext) throws {
         self.init(context: context)
-        insert(from: domain)
+        try insert(from: model)
     }
 
-    public func toDomain() -> DomainType {
+    public func toModel() -> ModelType {
         Keyword(
             id: id,
             noteId: voiceNote.id,
@@ -29,9 +29,9 @@ extension KeywordEntity: ManagedObjectMapping {
         )
     }
 
-    public func insert(from domain: DomainType) {
-        id = domain.id
-        word = domain.word
+    public func insert(from model: ModelType) throws {
+        id = model.id
+        word = model.word
     }
 
     public static var entityName: CoreDataEntityName {
@@ -40,31 +40,5 @@ extension KeywordEntity: ManagedObjectMapping {
 
     public static var sortDescriptors: [NSSortDescriptor] {
         [NSSortDescriptor(keyPath: \KeywordEntity.word, ascending: true)]
-    }
-
-    public static func identityPredicate(for domain: DomainType) -> NSPredicate {
-        NSPredicate(format: "id == %@", domain.id as CVarArg)
-    }
-
-    public static func identityPredicate(byId id: DomainType.ID) -> NSPredicate {
-        NSPredicate(format: "id == %@", id as CVarArg)
-    }
-
-    public static func find(for domain: DomainType, in context: NSManagedObjectContext) throws
-        -> Self?
-    {
-        let request = NSFetchRequest<Self>(entityName: entityName.rawValue)
-        request.predicate = identityPredicate(for: domain)
-        request.fetchLimit = 1
-        return try context.fetch(request).first
-    }
-
-    public static func find(byId id: DomainType.ID, in context: NSManagedObjectContext) throws
-        -> Self?
-    {
-        let request = NSFetchRequest<Self>(entityName: entityName.rawValue)
-        request.predicate = identityPredicate(byId: id)
-        request.fetchLimit = 1
-        return try context.fetch(request).first
     }
 }
--- Data/Sources/Infrastructure/CoreData/Entities/TranscriptEntity+CoreDataClass.swift
+++ Data/Sources/Infrastructure/CoreData/Entities/TranscriptEntity+CoreDataClass.swift
@@ -17,14 +17,14 @@ public final class TranscriptEntity: NSManagedObject {
 }
 
 extension TranscriptEntity: ManagedObjectMapping {
-    public typealias DomainType = Transcript
+    public typealias ModelType = Transcript
 
-    public convenience init(domain: DomainType, context: NSManagedObjectContext) {
+    public convenience init(model: ModelType, context: NSManagedObjectContext) throws {
         self.init(context: context)
-        insert(from: domain)
+        try insert(from: model)
     }
 
-    public func toDomain() -> DomainType {
+    public func toModel() -> ModelType {
         Transcript(
             id: id,
             createdAt: createdAt,
@@ -32,10 +32,10 @@ extension TranscriptEntity: ManagedObjectMapping {
         )
     }
 
-    public func insert(from domain: DomainType) {
-        id = domain.id
-        text = domain.text
-        createdAt = domain.createdAt
+    public func insert(from model: ModelType) throws {
+        id = model.id
+        text = model.text
+        createdAt = model.createdAt
     }
 
     public static var entityName: CoreDataEntityName {
@@ -44,31 +44,5 @@ extension TranscriptEntity: ManagedObjectMapping {
 
     public static var sortDescriptors: [NSSortDescriptor] {
         [NSSortDescriptor(keyPath: \TranscriptEntity.createdAt, ascending: true)]
-    }
-
-    public static func identityPredicate(for domain: DomainType) -> NSPredicate {
-        NSPredicate(format: "id == %@", domain.id as CVarArg)
-    }
-
-    public static func identityPredicate(byId id: DomainType.ID) -> NSPredicate {
-        NSPredicate(format: "id == %@", id as CVarArg)
-    }
-
-    public static func find(for domain: DomainType, in context: NSManagedObjectContext) throws
-        -> Self?
-    {
-        let request = NSFetchRequest<Self>(entityName: entityName.rawValue)
-        request.predicate = identityPredicate(for: domain)
-        request.fetchLimit = 1
-        return try context.fetch(request).first
-    }
-
-    public static func find(byId id: DomainType.ID, in context: NSManagedObjectContext) throws
-        -> Self?
-    {
-        let request = NSFetchRequest<Self>(entityName: entityName.rawValue)
-        request.predicate = identityPredicate(byId: id)
-        request.fetchLimit = 1
-        return try context.fetch(request).first
     }
 }
