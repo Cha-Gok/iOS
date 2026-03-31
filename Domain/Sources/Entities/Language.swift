e08701aac0a5150bc73582e94d997dbf56ce887c
김용해 <yoho1019@gmail.com>
2026-04-01 02:11:26 +0900

    refactor(domain): 충돌 회피 적용

--- Domain/Sources/Entities/Language.swift
+++ Domain/Sources/Entities/Language.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public enum Language: String, Sendable {
-  case ko = "Korean"
-  case en = "English"
+public enum Language: String, CaseIterable, Sendable {
+    case ko = "Korean"
+    case en = "English"
 }
