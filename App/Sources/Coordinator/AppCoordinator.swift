3df56356a3ee2b47fef8264857f228471513e912
김용해 <yoho1019@gmail.com>
2026-04-03 07:28:52 +0900

    feat(app): 화면 전환 Coordinator 구현 - BaseCoordinator를 상속 받는 객체로서 화면 전환 최상위 AppCoordinator 입니다. - UIView를 받는 과정을 캡슐화 하여 SceneDelegate의 가독성을 높입니다. - finishOnBoarding 화면 전환 처리를 delegate 패턴을 통해 처리합니다.

---
+++ App/Sources/Coordinator/AppCoordinator.swift
@@ -0,0 +1,56 @@
+import Presentation
+import UIKit
+
+@MainActor
+final class AppCoordinator: BaseCoordinator<UINavigationController> {
+    let window: UIWindow
+    let dependencyContainer: AppDIContainer = .init()
+
+    init(window: UIWindow) {
+        self.window = window
+        let presenter = UINavigationController()
+        presenter.isToolbarHidden = true
+        presenter.isNavigationBarHidden = true
+
+        super.init(presenter: presenter)
+        self.window.rootViewController = presenter
+        self.window.makeKeyAndVisible()
+    }
+
+    override func start() {
+        let firstUser: Bool = dependencyContainer.checkFirstLaunchUser()
+        if firstUser {
+            startOnBoarding()
+        } else {
+            startMain()
+        }
+    }
+
+    private func startOnBoarding() {
+        let onBoardingVC = dependencyContainer.makeOnBoardingViewController()
+        onBoardingVC.vm.navDelegate = self
+        presenter.setViewControllers([onBoardingVC], animated: false)
+    }
+
+    private func startMain() {
+        let mainVC = dependencyContainer.makeMainViewController()
+        presenter.setViewControllers([mainVC], animated: false)
+    }
+}
+
+// MARK: - Navigation Delegate
+
+extension AppCoordinator: NavigationDelegate {
+    /// 온보딩 화면에서 메인 화면으로 넘어가는 Navigation 함수
+    func finishOnBoarding() {
+        startMain()
+
+        UIView.transition(
+            with: window,
+            duration: 0.3,
+            options: .transitionCrossDissolve,
+            animations: nil,
+            completion: nil
+        )
+    }
+}
