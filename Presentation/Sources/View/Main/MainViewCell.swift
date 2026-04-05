1d1f020277d12225fa8d1bb1d4ffb75266c7aeb5
김용해 <yoho1019@gmail.com>
2026-04-05 18:09:01 +0900

    feat(presentation): MainVC 최소한의 Cell - CollectionView가 작동하기 위한 최소한의 Cell을 구성하였습니다.

---
+++ Presentation/Sources/View/Main/MainViewCell.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class MainViewCell: UICollectionViewCell {
+    static let reuseIdentifier: String = "MainViewCell"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
