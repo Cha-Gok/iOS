eddc054e02858bb75f0c896ae82e1d493af2530a
김용해 <yoho1019@gmail.com>
2026-04-05 18:03:29 +0900

    feat(presentation): UITabBarController의 화면 구성 - 작업의 편의성을 위해 미리 Folder, Recoding 관련 VC를 넣어두었습니다.

---
+++ Presentation/Sources/View/Folder/FolderViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class FolderViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.warning
+    }
+}
---
+++ Presentation/Sources/View/Recoding/RecordingViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class RecordingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.danger
+    }
+}
