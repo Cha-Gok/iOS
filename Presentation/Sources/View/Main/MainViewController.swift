63b1d4a1099957191654de6b895001fa6666d8c8
김용해 <yoho1019@gmail.com>
2026-04-11 19:40:52 +0900

    refactor(presentation): 배경 뷰 적용 및 버튼 리펙토링 - 메인 화면에 컴포넌트 GlassButton.floating을 연결합니다. - ViewController 내부에서 collectionView 사용의 경우 clear 해줘야 합니다.

--- Presentation/Sources/View/Main/MainViewController.swift
+++ Presentation/Sources/View/Main/MainViewController.swift
@@ -31,11 +31,13 @@ public final class MainViewController: ViewController {
         let layout = UICollectionViewFlowLayout()
         let c = UICollectionView(frame: .zero, collectionViewLayout: layout)
         c.translatesAutoresizingMaskIntoConstraints = false
-        c.backgroundColor = UIColor.gray50
+        c.backgroundColor = .clear
         return c
     }()
 
-    private let floatingButton: GlassButton = .primary("녹음 시작")
+    private let floatingButton: GlassButton = .floating(
+        image: .init(imageName: "microphone", type: .system)
+    )
 
     var dataSource: UICollectionViewDiffableDataSource<MainSection, MainCellItem>!
 
@@ -64,7 +66,6 @@ public final class MainViewController: ViewController {
     // MARK: Setup
 
     private func setup() {
-        view.backgroundColor = UIColor.gray50
         let appearance = UINavigationBarAppearance()
         appearance.configureWithTransparentBackground()
         appearance.backgroundColor = UIColor.gray50
