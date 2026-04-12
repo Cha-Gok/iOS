df8c5964edad64664fc4a62beaf2621bfe2e0d37
김용해 <yoho1019@gmail.com>
2026-04-12 21:38:31 +0900

    refactor(presentation): 메인 화면 List Cell 제거 - UIHostringConfiguration 방식으로 전환 합니다.

--- Presentation/Sources/Component/VoiceNote/VoiceNoteCardView.swift
+++ Presentation/Sources/Component/VoiceNote/VoiceNoteCardView.swift
@@ -6,36 +6,39 @@ struct VoiceNoteCardView: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 6) {
-            Text(title)
-                .foregroundStyle(.gray950)
-                .font(.system(size: 18))
-                .lineSpacing(1.3)
-            Text(subTitle)
-                .foregroundStyle(.gray800)
-                .font(.body)
-                .font(.system(size: 16))
-                .lineSpacing(1.5)
-                .tracking(-0.03)
-            Text("요약 필요")
-                .font(.system(size: 15))
-                .lineSpacing(1.3)
-                .tracking(-0.03)
-                .padding(.vertical, 4)
-                .padding(.horizontal, 12)
-                .overlay(
-                    Capsule()
-                        .stroke(Color.point600, lineWidth: 1)
-                )
-                .background(.point600.opacity(0.2), in: .capsule)
-                .foregroundStyle(.gray750)
+            Group {
+                Text(title)
+                    .foregroundStyle(.gray950)
+                    .font(.system(size: 18))
+                    .lineSpacing(1.3)
+                Text(subTitle)
+                    .foregroundStyle(.gray800)
+                    .font(.body)
+                    .font(.system(size: 16))
+                    .lineSpacing(1.5)
+                    .tracking(-0.03)
+                Text("요약 필요")
+                    .font(.system(size: 15))
+                    .lineSpacing(1.3)
+                    .tracking(-0.03)
+                    .padding(.vertical, 4)
+                    .padding(.horizontal, 12)
+                    .overlay(
+                        Capsule()
+                            .stroke(Color.point600, lineWidth: 1)
+                    )
+                    .background(.point600.opacity(0.2), in: .capsule)
+                    .foregroundStyle(.gray750)
+            }
         }
-        .frame(maxWidth: .infinity, maxHeight: 120, alignment: .leading)
         .frame(minHeight: 118)
-        .padding(.horizontal, 16)
+        .frame(maxWidth: .infinity, maxHeight: 120, alignment: .leading)
+        .padding(.leading)
+        .background(.point200.opacity(0.2))
         .glassEffect(.clear.interactive(), in: .rect(cornerRadius: 20))
     }
 }
 
 // #Preview {
-//    TrashListViewCell(title: "test", subTitle: "qqweqfq")
+//    VoiceNoteCardView(title: "test", subTitle: "qqweqfq")
 // }
