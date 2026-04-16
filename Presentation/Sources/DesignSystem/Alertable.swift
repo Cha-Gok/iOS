9da118e057568c37c42fe37f4a234aa651a9f22c
TomBumSuChoi <qjatn0545@naver.com>
2026-04-16 13:36:34 +0900

    refactor(presentation): Alertable에 title·onDismiss 오버로드 및 presentedViewController 가드 추가

--- Presentation/Sources/DesignSystem/Alertable.swift
+++ Presentation/Sources/DesignSystem/Alertable.swift
@@ -7,8 +7,16 @@ public protocol Alertable: UIViewController {
 
 public extension Alertable {
     func showAlert(message: String) {
+        guard presentedViewController == nil else { return }
         let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "확인", style: .default))
+        present(alert, animated: true)
+    }
+
+    func showAlert(title: String, message: String, onDismiss: @escaping () -> Void) {
+        guard presentedViewController == nil else { return }
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default) { _ in onDismiss() })
         present(alert, animated: true)
     }
 }
