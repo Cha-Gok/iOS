a5cb71a8ddb030cb8a921049477d89c13fe5ce6a
Kim yonghae <yoho1019@naver.com>
2026-04-17 18:33:31 +0900

    refactor(presentation): keyboard dismiss 제어 - alert 취소, 실행 버튼에 키보드를 닫는 함수 추가

--- Presentation/Sources/View/Folder/FolderViewController.swift
+++ Presentation/Sources/View/Folder/FolderViewController.swift
@@ -159,6 +159,7 @@ public final class FolderViewController: CollectionViewController {
         cancelButton.addAction(
             UIAction { [weak self] _ in
                 guard let self else { return }
+                textField.endEditing(true)
                 textField.field.text = ""
                 vm.closeTextField()
             },
@@ -177,6 +178,7 @@ public final class FolderViewController: CollectionViewController {
                 case .edit:
                     vm.update(name: name)
                 }
+                textField.endEditing(true)
                 textField.field.text = ""
             },
             for: .touchUpInside
