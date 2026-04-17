f07740ce5f01c2f181176f4b4a2d2b59ca55196b
TomBumSuChoi <qjatn0545@naver.com>
2026-04-17 20:25:27 +0900

    refactor(domain): playbackSkipInterval 10초에서 5초로 변경

--- Domain/Sources/Policy.swift
+++ Domain/Sources/Policy.swift
@@ -35,7 +35,7 @@ public enum Policy {
     public static let playbackProgressUpdateInterval: UInt64 = 100_000_000
 
     /// 재생 빨리감기/뒤로가기 이동 간격 (초)
-    public static let playbackSkipInterval: TimeInterval = 10
+    public static let playbackSkipInterval: TimeInterval = 5
 
     /// 세그먼트 간 공백이 이 값(초)을 초과하면 새 섹션으로 분리
     public static let scriptGroupingPauseThreshold: TimeInterval = 2.0
